862ed93df12a17a31bf77017d7e38388eca327f4
Kunal Sarkar <kunal.sarkar2810@gmail.com>
2025-08-23 04:30:37 +0530

    layout update in all pages

--- src/pages/MyOrdersPage.vue
+++ src/pages/MyOrdersPage.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <h1>My Orders Page</h1>
-  </div>
+  <Layout>
+    <section class="flex flex-col items-center justify-center py-8">
+      <h1 class="text-2xl font-medium mb-2">Orders</h1>
+      <nav class="text-sm">
+        <ol class="flex space-x-2">
+          <li>
+            <a href="/" class="text-orange hover:underline">Home</a>
+          </li>
+          <li>
+            <span>/</span>
+          </li>
+          <li>Orders</li>
+        </ol>
+      </nav>
+    </section>
+  </Layout>
 </template>
+
 <script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+import Layout from '../components/layout/Layout.vue';
+
+
+
 </script>
--- src/pages/OrderDetailsPage.vue
+++ src/pages/OrderDetailsPage.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
-    <h1>Order Details Page</h1>
-    <p>Order ID: {{ $route.params.orderId }}</p>
-  </div>
+  <Layout>
+    <section class="flex flex-col items-center justify-center py-8">
+      <h1 class="text-2xl font-medium mb-2">Orders ID</h1>
+      <nav class="text-sm">
+        <ol class="flex space-x-2">
+          <li>
+            <a href="/" class="text-orange hover:underline">Home</a>
+          </li>
+          <li>
+            <span>/</span>
+          </li>
+          <li>
+            <a href="#" class="text-orange hover:underline">Orders</a>
+          </li>
+          <li>
+            <span>/</span>
+          </li>
+          <li>Order ID</li>
+        </ol>
+      </nav>
+    </section>
+  </Layout>
 </template>
+
 <script setup lang="ts">
+import { ref, computed, onMounted } from 'vue';
+import Layout from '../components/layout/Layout.vue';
+
+
+
 </script>
